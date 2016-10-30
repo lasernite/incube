6e665246c8b5cf2740c854857b7e9cafc055e379
Laser Nite <lasernite@gmail.com>
2016-10-29 21:58:57 -0400

    basically complete, might want to add better images support and pull the projects into the home page, but essentially works

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -1,6 +1,6 @@
 module ProjectsHelper
 	def website_format(url)
-		if url[0..6] == "http://" or "https:/"
+		if url[0..6] == "http://" or url[0..6] == "https:/"
 			return url
 		else
 			return "http://" + url
